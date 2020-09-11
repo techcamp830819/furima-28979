43589d648bf8d96eb600a57df17389254409b0c2
techcamp830819 <odyssey3696@gmail.com>
2020-09-11 14:27:25 +0900

    ユーザー認証機能の実装2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
   end
 
   private
