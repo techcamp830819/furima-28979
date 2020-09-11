b1d6cb330fd9509132f59419cc5897c18ea24e83
techcamp830819 <odyssey3696@gmail.com>
2020-09-11 15:48:11 +0900

    email一意性の設定

--- db/migrate/20200911022102_devise_create_users.rb
+++ db/migrate/20200911022102_devise_create_users.rb
@@ -10,7 +10,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name_kana,      null: false
       t.string :first_name_kana,     null: false
       t.date :birth_date,            null: false
-      t.string :email,               null: false, default: ""
+      t.string :email,               null: false, default: "", uniqueness: true
       t.string :encrypted_password,  null: false, default: ""
 
       ## Recoverable
