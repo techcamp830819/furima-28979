7a0d910b84687f319a27d1e8351e5168807ccc45
techcamp830819 <odyssey3696@gmail.com>
2020-09-15 16:20:55 +0900

    ユーザー管理機能修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe User, type: :model do
       end
 
       it 'メールアドレスは@を含む必要があること' do
-        @user.email = '@'
+        @user.email = 'aaa.com'
         @user.valid?
         expect(@user.errors.full_messages).to include('Email is invalid')
       end
@@ -56,16 +56,26 @@ RSpec.describe User, type: :model do
 
       it 'ユーザー本名が、名字と名前がそれぞれ必須であること' do
         @user.last_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+
+      it 'ユーザー本名が、名字と名前がそれぞれ必須であること' do
         @user.first_name = nil
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name can't be blank", "First name can't be blank")
+        expect(@user.errors.full_messages).to include("First name can't be blank")
       end
 
       it 'ユーザー本名のフリガナが、名字と名前でそれぞれ必須であること' do
         @user.last_name_kana = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
+
+      it 'ユーザー本名のフリガナが、名字と名前でそれぞれ必須であること' do
         @user.first_name_kana = nil
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name kana can't be blank", "First name kana can't be blank")
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
       end
 
       it '生年月日が必須であること' do
