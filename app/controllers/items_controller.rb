b392d76a62c0f1892402fd01b84581cc56ee586d
techcamp830819 <odyssey3696@gmail.com>
2020-09-22 19:09:52 +0900

    商品削除機能実装修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,9 +34,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    if @item.destroy
     redirect_to root_path
+  else
+    render :show
   end
+end
 
   private
 
