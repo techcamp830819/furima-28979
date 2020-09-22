be9e4e2260f1f7224d4d785ecf8a2218b42b1624
techcamp830819 <odyssey3696@gmail.com>
2020-09-22 11:12:19 +0900

    商品詳細表示機能実装修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :handringtime_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :handringtime_id, :price, :image).merge(user_id: current_user.id)
   end
 end
